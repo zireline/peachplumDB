188bb0e273acb4a0411e1c0155dde6ae7e408bf3
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-08-18 20:53:52 +0800

    add action loggers

--- lib/service/PeachPlumService.ts
+++ lib/service/PeachPlumService.ts
@@ -3,7 +3,6 @@ import { CollectionManager } from '../io/manager/CollectionManager.js';
 import { JsonFileManager } from '../io/jsonfilemanager/JsonFileManager.js';
 import { AppDataDirProvider } from '../io/dir/AppDirProvider.js';
 import { JsonData } from '../io/jsonfilemanager/JsonData.js';
-import { DocData } from '../io/manager/DocData.js';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -26,102 +25,128 @@ app.use((req, res, next) => {
 // GET /collections/:collectionId/:docId
 app.get(
   '/collections/:collectionId/:docId',
-  async (
+  (
     req: { params: { collectionId: any; docId: any } },
     res: { send: (arg0: JsonData) => void; sendStatus: (arg0: number) => void }
   ) => {
+    console.info('[GET]', req);
+
     const { collectionId, docId } = req.params;
     const doc = collectionManager.collection(collectionId).doc(docId);
 
-    try {
-      const docData = await doc.get();
-      res.send(docData);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+    doc
+      .get()
+      .then((docData) => {
+        console.info(docData);
+        res.send(docData);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+        res.send(err);
+      });
   }
 );
 
 // GET /collections/:collectionId
 app.get(
   '/collections/:collectionId',
-  async (
+  (
     req: { params: { collectionId: any; docId: any } },
     res: { send: (arg0: JsonData) => void; sendStatus: (arg0: number) => void }
   ) => {
+    console.info('[GET]', req);
+
     const { collectionId } = req.params;
     const docs = collectionManager.collection(collectionId);
 
-    try {
-      const docData = await docs.read();
-      res.send(docData);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+    docs
+      .read()
+      .then((docData) => {
+        console.info(docData);
+        res.send(docData);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   }
 );
 
 // POST /collections/:collectionId/:docId
 app.post(
   '/collections/:collectionId/:docId',
-  async (
+  (
     req: { params: { collectionId: any; docId: any }; body: any },
     res: { sendStatus: (arg0: number) => void }
   ) => {
+    console.info('[POST]', req);
+
     const { collectionId, docId } = req.params;
     const docManager = collectionManager.collection(collectionId).doc(docId);
 
-    try {
-      await docManager.set(req.body);
-      res.sendStatus(201);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+    docManager
+      .set(req.body)
+      .then(() => {
+        console.info('posted: ' + req.body);
+        res.sendStatus(201);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.sendStatus(500);
+      });
   }
 );
 
 // PUT /collections/:collectionId/:docId
 app.put(
   '/collections/:collectionId/:docId',
-  async (
+  (
     req: { params: { collectionId: any; docId: any }; body: any },
     res: { sendStatus: (arg0: number) => void }
   ) => {
+    console.info('[PUT]', req);
+
     const { collectionId, docId } = req.params;
     const docManager = collectionManager.collection(collectionId).doc(docId);
 
-    try {
-      await docManager.update((data) => {
+    docManager
+      .update((data) => {
         Object.assign(data, req.body);
+      })
+      .then(() => {
+        console.info('updated: ' + req.body);
+        res.sendStatus(204);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.sendStatus(500);
       });
-      res.sendStatus(204);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
   }
 );
 
 // DELETE /collections/:collectionId/:docId
 app.delete(
   '/collections/:collectionId/:docId',
-  async (
+  (
     req: { params: { collectionId: any; docId: any } },
     res: { sendStatus: (arg0: number) => void }
   ) => {
+    console.info('[DELETE]', req);
+
     const { collectionId, docId } = req.params;
     const docManager = collectionManager.collection(collectionId).doc(docId);
 
-    try {
-      await docManager.delete();
-      res.sendStatus(204);
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+    docManager
+      .delete()
+      .then(() => {
+        console.info('deleted: ' + collectionId + '/' + docId);
+        res.sendStatus(204);
+      })
+      .catch((e) => {
+        console.error(e);
+        res.sendStatus(500);
+      });
   }
 );
 
@@ -129,6 +154,6 @@ export function startServer() {
   // Start the server
   const port = 6060;
   app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`[PeachPlumDB] Server listening on port ${port}`);
   });
 }
